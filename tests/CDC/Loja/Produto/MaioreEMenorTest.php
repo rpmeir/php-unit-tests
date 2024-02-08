a178d3dc593d2d36bbe8477f08a6672b0cecd543
Rodrigo Pires Meira <rpmeir@gmail.com>
2024-02-07 23:28:24 -0300

    chapter one

--- tests/CDC/Loja/Produto/MaioreEMenorTest.php
+++ tests/CDC/Loja/Produto/MaioreEMenorTest.php
@@ -4,39 +4,42 @@ use CDC\Loja\Carrinho\CarrinhoDeCompras;
 use CDC\Loja\Produto\MaiorEMenor;
 use CDC\Loja\Produto\Produto;
 
+const JOGO_DE_PRATOS = "Jogo de Pratos";
+const GELADEIRA = "Geladeira";
+
 test("testOrdemDecrescente", function () {
     $carrinho = new CarrinhoDeCompras();
-    $carrinho->adiciona(new Produto("Geladeira", 450.00));
+    $carrinho->adiciona(new Produto(GELADEIRA, 450.00));
     $carrinho->adiciona(new Produto("Liquididificador", 250.00));
-    $carrinho->adiciona(new Produto("Jogo de Pratos", 70.00));
+    $carrinho->adiciona(new Produto(JOGO_DE_PRATOS, 70.00));
 
     $maiorMenor = new MaiorEMenor();
     $maiorMenor->encontra($carrinho);
 
-    expect("Jogo de Pratos")->toBe($maiorMenor->getMenor()->getNome());
-    expect("Geladeira")->toBe($maiorMenor->getMaior()->getNome());
+    expect(JOGO_DE_PRATOS)->toBe($maiorMenor->getMenor()->getNome());
+    expect(GELADEIRA)->toBe($maiorMenor->getMaior()->getNome());
 });
 
 test("testOrdemVariada", function () {
     $carrinho = new CarrinhoDeCompras();
-    $carrinho->adiciona(new Produto("Geladeira", 450.00));
-    $carrinho->adiciona(new Produto("Jogo de Pratos", 70.00));
+    $carrinho->adiciona(new Produto(GELADEIRA, 450.00));
+    $carrinho->adiciona(new Produto(JOGO_DE_PRATOS, 70.00));
     $carrinho->adiciona(new Produto("Liquididificador", 250.00));
 
     $maiorMenor = new MaiorEMenor();
     $maiorMenor->encontra($carrinho);
 
-    expect("Jogo de Pratos")->toBe($maiorMenor->getMenor()->getNome());
-    expect("Geladeira")->toBe($maiorMenor->getMaior()->getNome());
+    expect(JOGO_DE_PRATOS)->toBe($maiorMenor->getMenor()->getNome());
+    expect(GELADEIRA)->toBe($maiorMenor->getMaior()->getNome());
 });
 
 test("testApenasUmProduto", function () {
     $carrinho = new CarrinhoDeCompras();
-    $carrinho->adiciona(new Produto("Geladeira", 450.00));
+    $carrinho->adiciona(new Produto(GELADEIRA, 450.00));
 
     $maiorMenor = new MaiorEMenor();
     $maiorMenor->encontra($carrinho);
 
-    expect("Geladeira")->toBe($maiorMenor->getMenor()->getNome());
-    expect("Geladeira")->toBe($maiorMenor->getMaior()->getNome());
+    expect(GELADEIRA)->toBe($maiorMenor->getMenor()->getNome());
+    expect(GELADEIRA)->toBe($maiorMenor->getMaior()->getNome());
 });
