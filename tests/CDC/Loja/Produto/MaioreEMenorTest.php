13c8f1a5a69f1e25438cadc7798d604f407a3a14
Rodrigo Pires Meira <rpmeir@gmail.com>
2024-02-07 23:21:58 -0300

    chapter one

--- tests/CDC/Loja/Produto/MaioreEMenorTest.php
+++ tests/CDC/Loja/Produto/MaioreEMenorTest.php
@@ -17,6 +17,19 @@ test("testOrdemDecrescente", function () {
     expect("Geladeira")->toBe($maiorMenor->getMaior()->getNome());
 });
 
+test("testOrdemVariada", function () {
+    $carrinho = new CarrinhoDeCompras();
+    $carrinho->adiciona(new Produto("Geladeira", 450.00));
+    $carrinho->adiciona(new Produto("Jogo de Pratos", 70.00));
+    $carrinho->adiciona(new Produto("Liquididificador", 250.00));
+
+    $maiorMenor = new MaiorEMenor();
+    $maiorMenor->encontra($carrinho);
+
+    expect("Jogo de Pratos")->toBe($maiorMenor->getMenor()->getNome());
+    expect("Geladeira")->toBe($maiorMenor->getMaior()->getNome());
+});
+
 test("testApenasUmProduto", function () {
     $carrinho = new CarrinhoDeCompras();
     $carrinho->adiciona(new Produto("Geladeira", 450.00));
